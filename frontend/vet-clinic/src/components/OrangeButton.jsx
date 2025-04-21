d05fe17ccff3466fb778a65311b7fa574e9e4626
Kotakub <kamabaxaxd@gmail.com>
2025-04-21 22:11:17 +0300

    Компонент оранжевой кнпоки

--- frontend/vet-clinic/src/components/OrangeButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OrangeButton({ buttonText }) {
-    return (
-        <button className='orangeButton'>
-            {buttonText}
-        </button>
-    )
-}
