02ee278d5d760d4c642cfafa73e9305d3036bfcf
Hendryjo <ruzillkomotov9@gmail.com>
2025-06-10 00:27:49 +0300

    save

---
+++ frontend/src/components/PetContainer/PetContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PetContainer() {
+    const [name, setName] = useState('');
+    const [gender, setNmae] = useState('');
+    const [breedId, setBreedId] = useState(0);
+    const [color, setColor] = useState('коричневый');
+    const [weight, setWeight] = useState(0);
+  return (
+    
+  )
+}
